c521e410fed35cbb7962bd37daf73a4acf707dd1
Nobeen Islam <40375177+NobeenIslam@users.noreply.github.com>
2022-05-30 20:08:52 +0100

    Fetched clients and employees

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,23 +1,42 @@
 import { useEffect, useState } from "react";
-import { ProjectInterface } from "../utils/Interfaces";
+import { ClientInterface, EmployeeInterface, ProjectInterface } from "../utils/Interfaces";
 import { ProjectCard } from "./ProjectCard";
 
 export function Dashboard(): JSX.Element {
   const [projects, setProjects] = useState<ProjectInterface[]>([]);
+  const [clients, setClients] = useState<ClientInterface[]>([]);
+  const [employees, setEmployees] = useState<EmployeeInterface[]>([]);
 
   async function fetchProjects() {
     const response = await fetch(
       "https://consulting-projects.academy-faculty.repl.co/api/projects"
     );
-    const jsonBody: ProjectInterface[] = await response.json();
-    setProjects(jsonBody);
+    const projectsJSON: ProjectInterface[] = await response.json();
+    setProjects(projectsJSON);
+  }
+
+  async function fetchClients() {
+    const response = await fetch(
+      "https://consulting-projects.academy-faculty.repl.co/api/clients"
+    );
+    const clientsJSON: ClientInterface[] = await response.json();
+    setClients(clientsJSON);
+  }
+
+  async function fetchEmployees() {
+    const response = await fetch(
+      "https://consulting-projects.academy-faculty.repl.co/api/employees"
+    );
+    const employeesJSON: ClientInterface[] = await response.json();
+    setClients(employeesJSON);
   }
 
   useEffect(() => {
     fetchProjects();
+    fetchClients();
+    fetchEmployees();
   }, []);
 
-  console.log(projects);
   return (
     <>
       <div>{JSON.stringify(projects[1])}</div>
