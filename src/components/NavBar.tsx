6c6952f148518f9a3c404de6c4b952c82f008d36
Nobeen Islam <40375177+NobeenIslam@users.noreply.github.com>
2022-05-31 18:37:13 +0100

    Formatting

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export function NavBar(): JSX.Element {
   return (
     <div className="navBar">
-      <div className = "homeLink">
-        <Link  to="/">Back to Dashboard</Link>
+      <div className="homeLink">
+        <Link to="/">Back to Dashboard</Link>
       </div>
       <div className="logo">Corpfolio</div>
     </div>
