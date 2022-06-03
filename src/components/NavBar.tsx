b2ed0c31fad5923a3d3104c4f7bfecfb545579b1
Nobeen Islam <40375177+NobeenIslam@users.noreply.github.com>
2022-06-03 16:08:12 +0100

    bootstrap navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,11 +2,15 @@ import { Link } from "react-router-dom";
 
 export function NavBar(): JSX.Element {
   return (
-    <div className="navBar">
-      <div className="homeLink">
-        <Link to="/">Back to Dashboard</Link>
+    <div className="navbar navbar-default navbar-light bg-light">
+      <div className="container-fluid">
+        <ul className="navbar-nav">
+          <li className="nav-item active nav-link">
+            <Link to="/">Back to Dashboard</Link>
+          </li>
+        </ul>
+        <div className="navbar-brand navbar-right">Corpfolio</div>
       </div>
-      <div className="logo">Corpfolio</div>
     </div>
   );
 }
